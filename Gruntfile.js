67a54f6860f03ea905ae268cb6fcbfade546b35a
Scott Mounce <scottmounce10@gmail.com>
2020-10-09 11:45:03 -0700

    got grunt file working with css 3000 is rendering off of links  from aws

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // concat: {
-    //   options : {
-    //     seperator: ';',
-    //   },
-    //   dist: {
-    //     src: ['Add-To-Cart/Dist/bundle.js', 'ImageCarousel/public/bundle.js', 'Reviews/public/bundle.js', 'Seller/client/dist/bundle.js'],
-    //     dest: 'dist/built.js'
-    //   }
-    // },
+    concat: {
+      options : {
+        seperator: ';',
+      },
+      dist: {
+        src: ['Add-To-Cart/Dist/bundle.js', 'ImageCarousel/public/bundle.js', 'Reviews/public/bundle.js', 'Seller/client/dist/bundle.js'],
+        dest: 'dist/built.js'
+      }
+    },
 
     cssmin: {
       options: {
@@ -18,16 +18,16 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'output.css': ['ImageCarousel/public/style.css', 'Reviews/public/style.css', 'Seller/client/dist/style.css']
+          'output.css': ['Seller/client/dist/style.css', 'Proxy/Public/styles.css']
         }
       }
     }      
   })
 
-  // grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin')
 
-  grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('default', ['cssmin', 'concat']);
   
 
 
